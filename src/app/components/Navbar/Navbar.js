ee3324a3bef3f845f1c0330af7094158782a1125
andrwsalcdo <asalcedo07@gmail.com>
2018-03-08 17:55:23 +0100

    add <Menu> with slide animation to navbar

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,18 +1,24 @@
 import React from 'react'
+import { slide as Menu } from "react-burger-menu"; 
 
 const Navbar = () => (
-    <header className="nav">
-        <ul className="nav-item-list">
-          <li className="nav-brand">
+  <header className="nav">
+    <Menu>
+      <li><a>How to Grill</a></li>
+      <li><a>Pricing</a></li>
+      <li><a>Book a Grill</a></li>  
+    </Menu>
+    <ul className="nav-item-list">
+        <li className="nav-brand">
             <a>GrillBer</a>
-          </li>
-          <li>
+        </li>
+        <li>
             <button className="btn-cta">
-              <a>Sign Up</a>
+            <a>Sign Up</a>
             </button>
-          </li>
-        </ul>
-    </header>
+        </li>
+    </ul>
+  </header>
 )
 
 export default Navbar; 
