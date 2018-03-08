e11dd4cde6702bea387178f5c6325bcfaf3f69a4
andrwsalcdo <asalcedo07@gmail.com>
2018-03-08 21:04:43 +0100

    Add href & style color to <a> in burger menu

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import { slide as Menu } from "react-burger-menu"; 
-import './styles.css'; 
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import './styles.css';
 
 const Navbar = () => (
   <header className="nav">
     <Menu>
-      <li><a>How to Grill</a></li>
-      <li><a>Pricing</a></li>
-      <li><a>Book a Grill</a></li>  
+      <li>
+        <a href="">How to Grill</a>
+      </li>
+      <li>
+        <a href="">Pricing</a>
+      </li>
+      <li>
+        <a href="">Book a Grill</a>
+      </li>
     </Menu>
     <ul className="nav-item-list">
-        <li className="nav-brand">
-          <a href="">GrillBer</a>
-        </li>
-        <li className="btn-cta">
-          <a href="">Sign Up</a>
-        </li>
+      <li className="nav-brand">
+        <a href="">GrillBer</a>
+      </li>
+      <li className="btn-cta">
+        <a href="">Sign Up</a>
+      </li>
     </ul>
   </header>
-)
+);
 
-export default Navbar; 
+export default Navbar;
