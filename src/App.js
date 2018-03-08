4be790f380605264adbba0ffa4c15a753aa12a7e
andrwsalcdo <asalcedo07@gmail.com>
2018-03-08 14:48:43 +0100

    change <App> into sfc from class component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-          This is my first PR. Let's get started. - Andrew
-        </p>
-        <p className="App-intro">
-          This is my first PR. Let's get started. -Yordan
-        </p>
-        <p className="App-intro">
-          This is my first PR. Let's go! - Fouad
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <p className="App-intro">
+      This is my first PR. Let's get started. - Andrew
+    </p>
+    <p className="App-intro">
+      This is my first PR. Let's get started. -Yordan
+    </p>
+    <p className="App-intro">
+      This is my first PR. Let's go! - Fouad
+    </p>
+  </div>
+)
 
 export default App;
