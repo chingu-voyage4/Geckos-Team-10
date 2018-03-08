44c68970919825b151cc41028760982b8c03a207
andrwsalcdo <asalcedo07@gmail.com>
2018-03-08 14:50:43 +0100

    delete boilerplate App.css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <p className="App-intro">
+  <div>
+    <p>
       This is my first PR. Let's get started. - Andrew
     </p>
-    <p className="App-intro">
+    <p>
       This is my first PR. Let's get started. -Yordan
     </p>
-    <p className="App-intro">
+    <p>
       This is my first PR. Let's go! - Fouad
     </p>
   </div>
