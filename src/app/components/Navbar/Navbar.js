3d2795ac46e6dcee2e6c58f4f95fe4a2b705bc81
andrwsalcdo <asalcedo07@gmail.com>
2018-03-08 17:40:26 +0100

    Add html skeleton for navbar

---
+++ src/app/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => (
+    <header className="nav">
+        <ul className="nav-item-list">
+          <li className="nav-brand">
+            <a>GrillBer</a>
+          </li>
+          <li>
+            <button className="btn-cta">
+              <a>Sign Up</a>
+            </button>
+          </li>
+        </ul>
+    </header>
+)
+
+export default Navbar; 
