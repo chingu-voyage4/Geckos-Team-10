d1159a26436e0a5c6dca60ccb3d14ee13c25ae06
andrwsalcdo <asalcedo07@gmail.com>
2018-03-10 20:17:28 +0100

    add multiple Links in Navbar component

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const Navbar = () => (
   <header className="nav">
     <Menu>
       <li>
-        <a href="">How to Grill</a>
+        <Link to="/how-to-grill">How to Grill</Link>
       </li>
       <li>
-        <a href="">Pricing</a>
+        <Link to="/pricing">Pricing</Link>
       </li>
       <li>
-        <a href="">Book a Grill</a>
+        <Link to="/book-a-grill">Book a Grill</Link>
       </li>
     </Menu>
     <nav className="nav-item-list">
       <ul className="nav-link-list">
-          <li className="nav-link"><a href="">How to Grill</a></li>
-          <li className="nav-link"><a href="">Pricing</a></li>
-          <li className="nav-link"><a href="">Book a Grill</a></li>
+        <li className="nav-link">
+          <Link to="/how-to-grill">How to Grill</Link>
+        </li>
+        <li className="nav-link">
+          <Link to="/pricing">Pricing</Link>
+        </li>
+        <li className="nav-link">
+          <Link to="/book-a-grill">Book a Grill</Link>
+        </li>
       </ul>
       <li className="nav-brand">
-        <a href="">GrillBer</a>
+        <Link to="/">GrillBer</Link>
       </li>
       <li className="btn-cta">
-        <a href="">Sign Up</a>
+        <Link to="/sign-up">Sign Up</Link>
       </li>
     </nav>
   </header>
