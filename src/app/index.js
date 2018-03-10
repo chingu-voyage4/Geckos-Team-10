180c08c5e5d28d61640b606cc1be3c0c7ec35974
andrwsalcdo <asalcedo07@gmail.com>
2018-03-10 22:15:37 +0100

    refactor: app into arrow function

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom'; 
-import PrimaryLayout from './layouts/PrimaryLayout'; 
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import PrimaryLayout from './layouts/PrimaryLayout';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <PrimaryLayout />
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <PrimaryLayout />
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
