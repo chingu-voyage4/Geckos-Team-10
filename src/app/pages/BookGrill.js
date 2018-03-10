583362b56a5a75a08a0d13c44ededf2b07baa9ca
andrwsalcdo <asalcedo07@gmail.com>
2018-03-10 21:27:25 +0100

    add h1 to Book a grill page

---
+++ src/app/pages/BookGrill.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const styles = {
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: '25vh'
+};
+
+const BookGrill = () => (
+  <div style={styles}>
+    <h1>This is the Book a Grill page</h1>
+  </div>
+);
+
+export default BookGrill;
