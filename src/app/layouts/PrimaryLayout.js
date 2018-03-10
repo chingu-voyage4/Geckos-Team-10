ba0b46d620a3c49b54eeeb4df0db91f1ace0ebd8
andrwsalcdo <asalcedo07@gmail.com>
2018-03-10 21:57:30 +0100

    import Switch, Route, and all pages

--- src/app/layouts/PrimaryLayout.js
+++ src/app/layouts/PrimaryLayout.js
@@ -1,12 +1,20 @@
-import React from 'react'; 
-import Navbar from '../components/Navbar/Navbar'; 
-import Footer from '../components/Footer/Footer'; 
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
+
+import LandingPage from '../pages/LandingPage';
+import BookGrill from '../pages/BookGrill';
+import HowToGrill from '../pages/HowToGrill';
+import Pricing from '../pages/Pricing';
+import SignUp from '../pages/SignUp';
 
 const PrimaryLayout = () => (
-    <div>
-        <Navbar />
-        <Footer />
-    </div>
-)
+  <div>
+    <Navbar />
+    <Footer />
+  </div>
+);
 
-export default PrimaryLayout; 
+export default PrimaryLayout;
