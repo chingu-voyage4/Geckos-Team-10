a80baafd5531830e2bde0d7ee5213c11e5f5878c
andrwsalcdo <asalcedo07@gmail.com>
2018-03-10 21:32:09 +0100

    add h1 to pricing page

---
+++ src/app/pages/Pricing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const styles = {
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: '25vh'
+};
+
+const Pricing = () => (
+  <div style={styles}>
+    <h1>This is the Pricing page</h1>
+  </div>
+);
+
+export default Pricing;
