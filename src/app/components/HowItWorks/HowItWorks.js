76af9ccce58c54f0a640790b98af68c97789ad60
andrwsalcdo <asalcedo07@gmail.com>
2018-03-11 19:32:08 +0100

    import <Card> & set className variables

--- src/app/components/HowItWorks/HowItWorks.js
+++ src/app/components/HowItWorks/HowItWorks.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Card from '../Card/Card';
+
+// class name variables 
+const card_wrap = 'card-wrap';
+const image_wrap = 'img-wrap';
+const text_wrap = 'text-wrap';
 
 const HowItWorks = () => (
-    <section>
-        <header>
-            <h2></h2>
-            <h2></h2>
-        </header>
-        <div>
-            {/* card components */}
-        </div>
-    </section>            
-)
+  <section>
+    <header>
+      <h2 />
+      <h2 />
+    </header>
+    <div>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </section>
+);
 
-export default HowItWorks; 
+export default HowItWorks;
