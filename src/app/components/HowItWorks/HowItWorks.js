9975259589b89407a8b93747fe7a8bf6d3bbb402
andrwsalcdo <asalcedo07@gmail.com>
2018-03-11 19:22:38 +0100

    add HowItWorks jsx markup

---
+++ src/app/components/HowItWorks/HowItWorks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HowItWorks = () => (
+    <section>
+        <header>
+            <h2></h2>
+            <h2></h2>
+        </header>
+        <div>
+            {/* card components */}
+        </div>
+    </section>            
+)
+
+export default HowItWorks; 
