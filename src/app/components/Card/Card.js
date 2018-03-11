e6f7f4ee8d72ffb8f4aa0bdc339ed33c292a07ec
andrwsalcdo <asalcedo07@gmail.com>
2018-03-11 18:41:42 +0100

    add <Card>'s jsx skeleton

---
+++ src/app/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+const Card = () => (
+    <div>
+       <div>
+           <img src="" alt=""/>
+        </div>
+       <div>
+           <h3></h3>
+           <p></p>
+       </div>        
+    </div>
+)
+
+export default Card; 
