3a23ac5c28e37ffaab8e0c3c0e3dec70ac2579e1
FOUAD TABBARA - U400515 <fmtabbara@hotmail.co.uk>
2018-03-14 14:25:52 +0000

    added reviews component for review

--- src/app/components/Reviews/Reviews.js
+++ src/app/components/Reviews/Reviews.js
@@ -1,43 +1,52 @@
-import React, { Component } from 'react';
-import './Reviews.css';
+import React, { Component } from "react";
+import "./Reviews.css";
 
 class Reviews extends Component {
   render() {
-    const reviewImagePh = 'https://www.texasmonthly.com/wp-content/uploads/2013/12/Kent-Black-profile.jpg';
     return (
       <div id="reviews-component">
-        <div id="title">
-          What our customers say
-        </div>
+        <div id="title">What our customers say</div>
         <div id="reviews-container">
           <div className="review-column">
-            <div className="review-image">
-            </div>
+            <div className="review-image-1" />
             <blockquote className="talk-bubble tri-right round right-in">
-              Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar
+              Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum
+              dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar
               <div id="star-rating">
-              <i className="fas fa-star star-fill"></i><i className="fas fa-star star-fill"></i><i class="fas fa-star star-fill"></i><i className="fas fa-star star-fill"></i><i className="fas fa-star star-fill"></i>
-            </div>
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+              </div>
             </blockquote>
           </div>
-           <div className="review-column">
-            <div className="review-image">
-            </div>
-             <blockquote className="talk-bubble tri-right round right-in">
-              Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar
-               <div id="star-rating">
-              <i className="fas fa-star star-fill"></i><i className="fas fa-star star-fill"></i><i class="fas fa-star star-fill"></i><i className="fas fa-star star-fill"></i><i className="fas fa-star star-fill"></i>
-            </div>
+          <div className="review-column">
+            <div className="review-image-2" />
+            <blockquote className="talk-bubble tri-right round right-in">
+              Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum
+              dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar
+              <div id="star-rating">
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+              </div>
             </blockquote>
           </div>
-           <div className="review-column">
-            <div className="review-image">
-            </div>
+          <div className="review-column">
+            <div className="review-image-3" />
             <blockquote className="talk-bubble tri-right round right-in">
-              Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar
+              Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum
+              dolar Lorem ipsum dolar Lorem ipsum dolar Lorem ipsum dolar
               <div id="star-rating">
-              <i className="fas fa-star star-fill"></i><i className="fas fa-star star-fill"></i><i className="fas fa-star star-fill"></i><i className="fas fa-star star-fill"></i><i className="fas fa-star star-no-fill"></i>
-            </div>
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-fill" />
+                <i className="fas fa-star star-no-fill" />
+              </div>
             </blockquote>
           </div>
         </div>
