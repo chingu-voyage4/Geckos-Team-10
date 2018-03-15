6cddfa0a5a1acde0b81a9a4ece3c248c37c623c8
Igor Katselenbogen <underwoodkats@gmail.com>
2018-03-15 19:47:38 +0300

    first work with waffle

--- src/app/pages/HowToGrill.js
+++ src/app/pages/HowToGrill.js
@@ -9,6 +9,7 @@ const styles = {
 const HowToGrill = () => (
   <main style={styles}>
     <h1>This is the How To Grill page</h1>
+      <h2>First you need to buy a pork or a beef in case if you dont like to eat a pork</h2>
   </main>
 );
 
