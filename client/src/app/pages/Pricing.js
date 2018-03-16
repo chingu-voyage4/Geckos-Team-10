5f0e48d6e7dd82c3e7306a72a5b64efabca1562f
andrwsalcdo <asalcedo07@gmail.com>
2018-03-16 20:41:27 +0100

    update pricing page with a call to server api/hello example

--- client/src/app/pages/Pricing.js
+++ client/src/app/pages/Pricing.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { Component } from 'react';
+
+class Pricing extends Component {
+  state = {
+    serverMsg: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ serverMsg: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <main style={styles}>
+        <h1>this is the pricing page</h1>
+        <h2>{this.state.serverMsg}</h2>
+      </main>
+    );
+  }
+}
 
 const styles = {
-  display: "flex",
-  justifyContent: "center",
-  marginTop: "25vh"
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  marginTop: '120px'
 };
-
-const Pricing = () => (
-  <main style={styles}>
-    <h1>This is the Pricing page</h1>
-  </main>
-);
 
 export default Pricing;
