074bff173faa359b1df32c98ae2f7091af0dab82
andrwsalcdo <asalcedo07@gmail.com>
2018-03-16 20:36:50 +0100

    Start simple server & get req from api/hello

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'This is the server, Express. How are you?' });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
