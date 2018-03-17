b619110bde21d84836b90d8ccad9449bbd0aaa96
andrwsalcdo <asalcedo07@gmail.com>
2018-03-17 18:28:50 +0100

    import styles, update classNames in Card

--- client/src/app/components/Card/Card.js
+++ client/src/app/components/Card/Card.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './styles.css'; 
 
 const Card = ({ cardWrap, imgWrap, src, alt, textWrap, title, text }) => (
-  <div className={cardWrap}>
-    <div className={imgWrap}>
+  <div className={`card-wrap ${cardWrap}`}>
+    <div className={`card-img-wrap ${imgWrap}`}>
       <img src={src} alt={alt} />
     </div>
-    <div className={textWrap}>
+    <div className={`card-text-wrap ${textWrap}`}>
       <h3>{title}</h3>
       <p>{text}</p>
     </div>
