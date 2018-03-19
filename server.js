c5f527a8800f3de45c36c0f637c778d05df2afca
andrwsalcdo <asalcedo07@gmail.com>
2018-03-19 17:20:03 +0100

    change server & proxy port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 4001;
 
 app.get("/api/hello", (req, res) => {
   res.send({ express: "This is the server, Express. How are you?" });
