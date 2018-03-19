399c1d2638b2808b7739079d976dc2977a68db5d
andrwsalcdo <asalcedo07@gmail.com>
2018-03-19 23:49:06 +0100

    refactor: CDM in pricing pg to use fetch w/ promises

--- client/src/app/pages/Pricing.js
+++ client/src/app/pages/Pricing.js
@@ -1,24 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Pricing extends Component {
   state = {
-    serverMsg: ''
+    serverMsg: ""
   };
 
   componentDidMount() {
-    this.callApi()
+    fetch("/api/hello")
+      .then(res => res.json())
       .then(res => this.setState({ serverMsg: res.express }))
       .catch(err => console.log(err));
   }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
 
   render() {
     return (
@@ -31,11 +23,11 @@ class Pricing extends Component {
 }
 
 const styles = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  marginTop: '120px'
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  marginTop: "120px"
 };
 
 export default Pricing;
