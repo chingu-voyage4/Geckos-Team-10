1028cae71fdbf717956dd6d01d3aea70cbd3c626
yordanzhelevdev <yordanzhelev950711@gmail.com>
2018-03-19 02:35:42 +0200

    Added all of the icons for the "How it works" steps

--- client/src/app/pages/HowToGrill.js
+++ client/src/app/pages/HowToGrill.js
@@ -1,10 +1,20 @@
 import React from "react";
 import SlideHeader from "../components/SlideHeader/SlideHeader";
+import Card from "../components/Card/Card";
+import SubHeader from "../components/SubHeader/SubHeader";
 
 //Images for the slider
 import hgrill from "../images/hgrill.jpg";
 import hgrill1 from "../images/hgrill1.jpg";
 import hgrill2 from "../images/hgrill2.jpg";
+
+//Images for the cards
+import agenda from "../images/agenda.svg";
+import barbecue from "../images/barbecue.svg";
+import receive from "../images/receive.svg";
+import returnT from "../images/returnT.svg";
+import screen from "../images/screen.svg";
+import reload from "../images/reload.svg";
 
 const images = [hgrill, hgrill1, hgrill2];
 const subheading = `
@@ -21,6 +31,51 @@ const HowToGrill = () => (
       heading="How does GrillBer work?"
       subheading={subheading}
     />
+
+    <div className="htg-container">
+      <SubHeader h1="How it works" h2="Five simple steps" />
+
+      <Card
+        src={screen}
+        alt="desktop monitor and mobile device"
+        title="Join"
+        text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+      />
+      <Card
+        src={agenda}
+        alt="book with bookmark"
+        title="Book"
+        text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+      />
+
+      <Card
+        src={receive}
+        alt="truck"
+        title="Receive"
+        text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+      />
+
+      <Card
+        src={barbecue}
+        alt="bbq grill"
+        title="Grill"
+        text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+      />
+
+      <Card
+        src={returnT}
+        alt="truck"
+        title="Return"
+        text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+      />
+
+      <Card
+        src={reload}
+        alt="reaload"
+        title="Reload!"
+        text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+      />
+    </div>
   </main>
 );
 
