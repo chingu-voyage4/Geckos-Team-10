9b49b822ead1b984febd1a1450699f21ad40b4d8
Igor Katselenbogen <underwoodkats@gmail.com>
2018-03-20 19:37:12 +0300

    132-changed-pricing

--- src/app/pages/Pricing.js
+++ src/app/pages/Pricing.js
@@ -2,16 +2,15 @@ import React from "react";
 import Price from "../components/Price/Price";
 
 const styles = {
-    display: "flex",
-    justifyContent: "center",
-    marginTop: "25vh"
+  display: "flex",
+  justifyContent: "center",
+  marginTop: "25vh"
 };
 
 const Pricing = () => (
-<main style={styles}>
-    <h1>This is the Pricing page</h1>
-<Price />
-</main>
+  <main style={styles}>
+    <Price />
+  </main>
 );
 
 export default Pricing;
