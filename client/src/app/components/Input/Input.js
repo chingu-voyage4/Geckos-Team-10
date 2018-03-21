4dca776199c4735273b7d2541a20c130fd2b493e
andrwsalcdo <asalcedo07@gmail.com>
2018-03-21 18:56:53 +0100

    add input component jsx skeleton w/ props

---
+++ client/src/app/components/Input/Input.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Input = ({
+  inputWrap,
+  label,
+  inputType,
+  placeholder,
+  icon = null
+  // value
+  // onChange
+}) => (
+  <div className={`input-container ${inputWrap}`}>
+    <label for={label}>{label}</label>
+    <input
+      type={inputType}
+      id={label}
+      placeholder={placeholder}
+      /*onChange, value */ required
+    />
+    {icon === null ? null : <i className={icon} />}
+  </div>
+);
+
+export default Input;
