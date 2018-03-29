a86269522e5eaf91a537669243d09380363bf824
andrwsalcdo <asalcedo07@gmail.com>
2018-03-29 18:02:54 +0200

    add apollo client and provider to app component

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import PrimaryLayout from "./layouts/PrimaryLayout";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+//default uri is /graphql
+const client = new ApolloClient({});
 
 const App = () => (
-  <BrowserRouter>
-    <div>
-      <PrimaryLayout />
-    </div>
-  </BrowserRouter>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <div>
+        <PrimaryLayout />
+      </div>
+    </BrowserRouter>
+  </ApolloProvider>
 );
 
 export default App;
