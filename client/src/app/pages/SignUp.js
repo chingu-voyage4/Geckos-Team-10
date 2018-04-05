b13859ff57c2020a1a93395c3ebd9ca38d245c15
andrwsalcdo <asalcedo07@gmail.com>
2018-04-05 22:24:35 +0200

    refactor: signUp page to include <UserForm />

--- client/src/app/pages/SignUp.js
+++ client/src/app/pages/SignUp.js
@@ -1,97 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import Input from "../components/Input/Input";
+import UserForm from "../components/UserForm/UserForm";
 import signupLogo from "../images/signup_logo.svg";
 
-class SignUp extends Component {
-  state = {
-    first_name: "",
-    last_name: "",
-    mobile_number: "",
-    email: "",
-    password: ""
-  };
+const SignUp = () => (
+  <main styles={styles.main}>
+    <img
+      src={signupLogo}
+      style={styles.logo}
+      alt="grill with vegetables and pork"
+    />
+    <UserForm />
+    <p style={styles.terms}>
+      By clicking "Sign Up", you agree to Grillber's Terms and Conditions and
+      Privacy Policy.
+    </p>
+  </main>
+);
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  // todo: onSubmit handler
-  onSubmit = e => {
-    alert("hello");
-    e.preventDefault();
-  };
-
-  render() {
-    return (
-      <main style={styles.main}>
-        <img
-          src={signupLogo}
-          style={styles.logo}
-          alt="grill with vegetables and pork"
-        />
-        <form style={styles.form} onSubmit={this.onSubmit}>
-          <div style={styles.full_name_wrap}>
-            <Input
-              label="First Name"
-              inputType="text"
-              placeholder="First Name"
-              name="first_name"
-              onChange={this.handleChange}
-              value={this.state.first_name}
-            />
-            <Input
-              label="Last Name"
-              inputType="text"
-              placeholder="Last Name"
-              name="last_name"
-              onChange={this.handleChange}
-              value={this.state.last_name}
-            />
-          </div>
-          <Input
-            label="Mobile Number"
-            type="tel"
-            placeholder="01-123-555-1234"
-            icon="fas fa-mobile-alt"
-            name="mobile_number"
-            onChange={this.handleChange}
-            value={this.state.mobile_number}
-          />
-          <Input
-            label="Email"
-            type="email"
-            placeholder="email@example.com"
-            icon="fas fa-envelope"
-            name="email"
-            onChange={this.handleChange}
-            value={this.state.email}
-          />
-          <Input
-            label="Password"
-            type="password"
-            placeholder="Use Fake Password"
-            icon="fas fa-lock"
-            name="password"
-            onChange={this.handleChange}
-            value={this.state.password}
-          />
-          <button type="submit" className="cta-join cta-sign-up">
-            <span>SIGN UP</span>
-            <i className="fas fa-arrow-right" />
-          </button>
-        </form>
-        <p style={styles.terms}>
-          By clicking "Sign Up", you agree to Grillber's Terms and Conditions
-          and Privacy Policy.
-        </p>
-      </main>
-    );
-  }
-}
-
-// transfer to own css file
 const styles = {
   main: {
     display: "flex",
@@ -99,19 +25,6 @@ const styles = {
     alignItems: "center",
     justifyContent: "center",
     marginBottom: "50px"
-  },
-  form: {
-    display: "flex",
-    flexDirection: "column"
-  },
-  full_name_wrap: {
-    display: "flex",
-    flexDirection: "row",
-    marginBottom: "10px"
-  },
-  terms: {
-    fontSize: "13px",
-    color: "black"
   },
   logo: {
     margin: "40px 0 20px 0",
