cdcb71850c1b9cfae007c1c06e290e0629a8da39
andrwsalcdo <asalcedo07@gmail.com>
2018-04-06 21:18:28 +0200

    add <Query /> to retrieve grill data for grill list

--- client/src/app/pages/BookGrill.js
+++ client/src/app/pages/BookGrill.js
@@ -1,16 +1,27 @@
 import React from "react";
-import GrillList from "../components/GrillList/GrillList";
+import GrillList from "../components/Grill/GrillList";
+import { Query } from "react-apollo";
+import { GET_GRILLS } from "../graphql/queries";
+
+const BookGrill = () => (
+  <main style={styles}>
+    <Query query={GET_GRILLS}>
+      {({ data, loading, error }) => {
+        if (loading) return <h1>loading...</h1>;
+        if (error) return <h1>error...</h1>;
+
+        const { grills } = data;
+
+        return <GrillList grills={grills} />;
+      }}
+    </Query>
+  </main>
+);
 
 const styles = {
   display: "flex",
   justifyContent: "center",
   marginTop: "25vh"
 };
-
-const BookGrill = () => (
-  <main style={styles}>
-    <h1>this is the book a grill page</h1>
-  </main>
-);
 
 export default BookGrill;
