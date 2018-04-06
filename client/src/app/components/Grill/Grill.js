5ea34799cfcbb0874d928f5ccf7c44d9a1aa74f8
andrwsalcdo <asalcedo07@gmail.com>
2018-04-06 20:09:54 +0200

    refactor <Grill /> for grill data

--- client/src/app/components/Grill/Grill.js
+++ client/src/app/components/Grill/Grill.js
@@ -2,40 +2,32 @@ import React from "react";
 import "./styles.css";
 
 const starImg = <i className="fas fa-star star-fill" />;
-const Grill = ({
-  grillImage,
-  grillName,
-  cost,
-  start,
-  end,
-  stars,
-  feature1,
-  feature2
-}) => (
+
+const Grill = ({ grill }) => (
   <div className="grill">
     <div className="grill-overview">
       <div className="grill-image">
-        <img
-          className="grill-img"
-          src="http://pngimg.com/uploads/grill/grill_PNG13965.png"
-        />
+        <img className="grill-img" src={grill.image} />
       </div>
       <div className="star-rating">
-        {starImg} {stars}/5
+        {starImg} {grill.rating}/5
       </div>
     </div>
     <div className="grill-detail">
-      <div className="grill-name">{grillName}</div>
+      <div className="grill-name">{grill.name}</div>
       <div className="grill-features">
         <ul className="features-list">
-          <li className="feature">{feature1}</li>
-          <li className="feature">{feature2}</li>
+          {grill.features.map(feature => (
+            <li className="feature" key={feature.main}>
+              {feature.main}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
     <div className="cost">
-      {cost}
-      <span className="timeframe">per day</span>
+      {grill.price_per_hour}
+      <span className="timeframe">per hour</span>
     </div>
   </div>
 );
