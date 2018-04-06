c4223be387e1bdd40cacbbf4d1f3cf3b748b35af
andrwsalcdo <asalcedo07@gmail.com>
2018-04-06 21:04:15 +0200

    fix: Grill import path & map over grills data in GrillList

--- client/src/app/components/Grill/GrillList.js
+++ client/src/app/components/Grill/GrillList.js
@@ -1,21 +1,9 @@
 import React from "react";
-import Grill from "../Grill/Grill";
-import "./styles.css";
+import Grill from "./Grill/Grill";
 
 const GrillList = ({ grills }) => (
   <section style={styles.results}>
-    <div>
-      <Grill
-        grillImage="Grill Image"
-        grillName="THE MINI"
-        cost="£9.95"
-        start="Weds 28 Mar - 9am"
-        end="Sat 31 Mar - 9am"
-        stars="3.5"
-        feature1="feature-1"
-        feature2="feature-2"
-      />
-    </div>
+    <div>{grills.map(grill => <Grill grill={grill} />)}</div>
   </section>
 );
 
