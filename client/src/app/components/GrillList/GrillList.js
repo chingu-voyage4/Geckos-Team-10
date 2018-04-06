5fd9509087cefe36f7f7dfec324e12ccdf671b08
andrwsalcdo <asalcedo07@gmail.com>
2018-04-06 20:49:22 +0200

    Revert "refactor styles & map over grills for <Grill />"
    This reverts commit 2cd05aa4eb6ac9012b696c806bb1f6e564b1e528.

--- client/src/app/components/GrillList/GrillList.js
+++ client/src/app/components/GrillList/GrillList.js
@@ -1,19 +1,23 @@
 import React from "react";
 import Grill from "../Grill/Grill";
+import "./styles.css";
 
-const GrillList = ({ grills }) => (
-  <section style={styles.results}>
-    <div>{grills.map(grill => <Grill grill={grill} />)}</div>
+const GrillList = ({ noGrills }) => (
+  <section id="grill-results">
+    <div id="total-results">{noGrills}</div>
+    <div id="results-container">
+      <Grill
+        grillImage="Grill Image"
+        grillName="THE MINI"
+        cost="£9.95"
+        start="Weds 28 Mar - 9am"
+        end="Sat 31 Mar - 9am"
+        stars="3.5"
+        feature1="feature-1"
+        feature2="feature-2"
+      />
+    </div>
   </section>
 );
-
-const styles = {
-  results: {
-    display: "flex",
-    flexDirection: "column",
-    padding: "50px",
-    margin: "auto"
-  }
-};
 
 export default GrillList;
