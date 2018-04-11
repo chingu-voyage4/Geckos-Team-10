daf074790a1077f62dedd86d66bfd638afc49514
andrwsalcdo <asalcedo07@gmail.com>
2018-04-11 19:32:43 +0200

    Add <Link> to <GrillItem /> to SingleGrill page

--- client/src/app/components/Grill/Grill_Item/Grill_Item.js
+++ client/src/app/components/Grill/Grill_Item/Grill_Item.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 const starImg = <i className="fas fa-star star-fill" />;
@@ -6,32 +7,34 @@ const starImg = <i className="fas fa-star star-fill" />;
 const numberOfStars = rating => Array.from({ length: rating }, () => starImg);
 
 const GrillItem = ({ grill }) => (
-  <div className="grill">
-    <div className="grill-overview">
-      <div className="grill-image">
-        <img
-          className="grill-img"
-          src={grill.image}
-          alt={`thumbnail of ${grill.name}`}
-        />
+  <Link to={`book-a-grill/${grill._id}`}>
+    <div className="grill">
+      <div className="grill-overview">
+        <div className="grill-image">
+          <img
+            className="grill-img"
+            src={grill.image}
+            alt={`thumbnail of ${grill.name}`}
+          />
+        </div>
+        <div className="star-rating">{numberOfStars(grill.rating)}</div>
       </div>
-      <div className="star-rating">{numberOfStars(grill.rating)}</div>
+      <div className="grill-detail">
+        <div className="grill-name">{grill.name}</div>
+        <ul className="features-list">
+          {grill.features.map(feature => (
+            <li className="feature" key={feature.main}>
+              {feature.main}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="cost">
+        {grill.price_per_hour}
+        <span className="timeframe">per hour</span>
+      </div>
     </div>
-    <div className="grill-detail">
-      <div className="grill-name">{grill.name}</div>
-      <ul className="features-list">
-        {grill.features.map(feature => (
-          <li className="feature" key={feature.main}>
-            {feature.main}
-          </li>
-        ))}
-      </ul>
-    </div>
-    <div className="cost">
-      {grill.price_per_hour}
-      <span className="timeframe">per hour</span>
-    </div>
-  </div>
+  </Link>
 );
 
 export default GrillItem;
