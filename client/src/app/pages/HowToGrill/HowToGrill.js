cd53981e990f4910c9151523c10029dffa9bd181
andrwsalcdo <asalcedo07@gmail.com>
2018-04-12 11:24:35 +0200

    Revert "there was component in pages file. it was weird and i delepted it"
    This reverts commit c6e6a431843eb89bb2b590fd6a2eea8d2201b619.

---
+++ client/src/app/pages/HowToGrill/HowToGrill.js
@@ -0,0 +1,90 @@
+import React from "react";
+import SlideHeader from "../../components/SlideHeader/SlideHeader";
+import Card from "../../components/Card/Card";
+import GrillberRules from "../../components/GrillberRules/GrillberRules";
+import SubHeader from "../../components/SubHeader/SubHeader";
+import "./HowToGrill.css";
+
+//Images for the slider
+import hgrill from "../../images/hgrill.jpg";
+import hgrill1 from "../../images/hgrill1.jpg";
+import hgrill2 from "../../images/hgrill2.jpg";
+
+//Images for the cards
+import agenda from "../../images/agenda.svg";
+import barbecue from "../../images/barbecue.svg";
+import receive from "../../images/receive.svg";
+import returnT from "../../images/returnT.svg";
+import screen from "../../images/screen.svg";
+import reload from "../../images/reload.svg";
+
+const images = [hgrill, hgrill1, hgrill2];
+const subheading = `
+	Proin porttitor ultricies odio sed convallis.
+	Duis ligula dolor, auctor vitae lobortis at, consectetur quis sapien.
+	Suspendisse mollis lorem quis justo dictum mattis.
+	Donec at tincidunt arcu, eu mollis nisi. Sed mollis accumsan sapien, gravida pretium sapien scelerisque eu.
+`;
+
+const HowToGrill = () => (
+  <main>
+    <SlideHeader
+      images={images}
+      heading="How does GrillBer work?"
+      subheading={subheading}
+      wrapperClass="htg-slide-header"
+    />
+
+    <section className="htg-section">
+      <SubHeader h1="How it works" h2="Five simple steps" />
+
+      <div className="htg-card-container">
+        <Card
+          src={screen}
+          alt="desktop monitor and mobile device"
+          title="Join"
+          text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+        />
+        <Card
+          src={agenda}
+          alt="book with bookmark"
+          title="Book"
+          text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+        />
+
+        <Card
+          src={receive}
+          alt="truck"
+          title="Receive"
+          text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+        />
+
+        <Card
+          src={barbecue}
+          alt="bbq grill"
+          title="Grill"
+          text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+        />
+
+        <Card
+          src={returnT}
+          alt="truck"
+          title="Return"
+          text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+        />
+
+        <Card
+          src={reload}
+          alt="reaload"
+          title="Reload!"
+          text="Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus."
+        />
+      </div>
+    </section>
+    <section>
+      <GrillberRules />
+    </section>
+  </main>
+);
+
+export default HowToGrill;
