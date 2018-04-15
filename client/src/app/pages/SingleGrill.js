13027ba0f1dcc1ec6a4bb1b81f0ab4d61f86804f
andrwsalcdo <asalcedo07@gmail.com>
2018-04-15 19:33:03 +0200

    add <Query /> & <GrillDetail /> to SingleGrill

--- client/src/app/pages/SingleGrill.js
+++ client/src/app/pages/SingleGrill.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { GET_GRILL } from "../graphql/queries";
+import { Query } from "react-apollo";
+import Loader from "../components/Loader/Loader";
+import GrillDetail from "../components/Grill/GrillDetail/GrillDetail";
 
-const SingleGrill = () => (
-  <div>
-    <h1>this is the single grill page</h1>
-  </div>
+const SingleGrill = ({ match: { params: { _id } } }) => (
+  <main style={styles}>
+    <Query query={GET_GRILL} variables={{ _id }}>
+      {({ data, loading, error }) => {
+        if (loading) return <Loader message="Your Grill is loading..." />;
+        if (error) return <h1>Error with finding your grill...</h1>;
+
+        const { grill } = data;
+
+        return <GrillDetail grill={grill} />;
+      }}
+    </Query>
+  </main>
 );
+
+const styles = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  margin: "15vh 0"
+};
 
 export default SingleGrill;
