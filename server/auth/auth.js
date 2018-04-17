081f6d26fe3b3f3b83f42f66763aefff2cbd3704
andrwsalcdo <asalcedo07@gmail.com>
2018-04-17 20:51:50 +0200

    add refreshTokens()

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -24,3 +24,49 @@ export const createTokens = async (user, secret, secret2) => {
 
   return Promise.all([createToken, createRefreshToken]);
 };
+
+export const refreshTokens = async (
+  token,
+  refreshToken,
+  user_model,
+  SECRET,
+  SECRET_2
+) => {
+  let userId = -1;
+  try {
+    const { user: { _id } } = jwt.decode(refreshToken);
+    userId = _id;
+  } catch (err) {
+    return {};
+  }
+
+  if (!userId) {
+    return {};
+  }
+
+  const user = await user_model.findOne({ _id });
+
+  if (!user) {
+    return {};
+  }
+
+  const refreshSecret = SECRET_2 + user.password;
+
+  try {
+    jwt.verify(refreshToken, refreshSecret);
+  } catch (err) {
+    return {};
+  }
+
+  const [newToken, newRefreshToken] = await createTokens(
+    user,
+    SECRET,
+    refreshSecret
+  );
+
+  return {
+    token: newToken,
+    refreshToken: newRefreshToken,
+    user
+  };
+};
