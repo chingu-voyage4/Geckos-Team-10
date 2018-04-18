190e1f5b6bff6e7326e7f572907634fa2e53316c
andrwsalcdo <asalcedo07@gmail.com>
2018-04-18 18:59:23 +0200

    add AuthRoute for GrillLayout in primaryLayout

--- client/src/app/layouts/PrimaryLayout.js
+++ client/src/app/layouts/PrimaryLayout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import GrillLayout from "./GrillLayout";
+import { AuthRoute } from "../auth/auth";
 
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
 import LandingPage from "../pages/LandingPage";
-import BookGrill from "../pages/BookGrill/BookGrill";
-import SingleGrill from "../pages/SingleGrill/SingleGrill";
 import HowToGrill from "../pages/HowToGrill/HowToGrill";
 import Pricing from "../pages/Pricing/Pricing";
 import SignUp from "../pages/SignUp/SignUp";
@@ -20,8 +20,7 @@ const PrimaryLayout = () => (
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/how-to-grill" component={HowToGrill} />
       <Route exact path="/pricing" component={Pricing} />
-      <Route exact path="/book-a-grill" component={BookGrill} />
-      <Route exact path="/book-a-grill/:_id" component={SingleGrill} />
+      <AuthRoute path="/auth/book-a-grill" component={GrillLayout} />
       <Route exact path="/sign-up" component={SignUp} />
       <Route exact path="/sorry-not-auth" component={NotAuthUser} />
       <Route component={NoMatch} />
