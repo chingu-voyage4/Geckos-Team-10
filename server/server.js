ec451d7365f36e42aae2c5fec8ac61dfea6ebd58
andrwsalcdo <asalcedo07@gmail.com>
2018-04-18 17:00:13 +0200

    add: addUser() for tokens, and use cors

--- server/server.js
+++ server/server.js
@@ -31,6 +31,35 @@ mongoose.connection.on(
   console.error.bind(console, "mongo connection error....")
 );
 
+const addUser = async (req, res, next) => {
+  const token = req.headers["x-token"];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (err) {
+      const refreshToken = req.headers["x-refresh-token"];
+      const newTokens = await refreshTokens(
+        token,
+        refreshToken,
+        UserModel,
+        SECRET,
+        SECRET_2
+      );
+      if (newTokens.token && newTokens.refreshToken) {
+        res.set("Access-Control-Expose-Headers", "x-token, x-refresh-token");
+        res.set("x-token", newTokens.token);
+        res.set("x-refresh-token", newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
+
+app.use(cors("*"));
+app.use(addUser);
+
 // The GraphQL endpoint
 app.use(
   "/graphql",
