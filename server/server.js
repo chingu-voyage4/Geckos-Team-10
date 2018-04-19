6ad649c1f107dc836d78ca9aa8ea4faf83208c3d
andrwsalcdo <asalcedo07@gmail.com>
2018-04-19 15:40:48 +0200

    render static assets with CRA in server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,14 @@ const SECRET_2 = process.env.SECRET_2;
 const app = express();
 const port = 4001;
 
+// serve static assets from
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+});
+
 mongoose.connect(process.env.MONGO_URI, { dbName: "grillber" }, function(
   error
 ) {
